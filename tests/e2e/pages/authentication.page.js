7ed7d40f8f6085304f17fe2010fa1ec737a2ab8a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2015-10-24 19:34:43 -0200

    Improved user authentication page to avoid dumb requests.

--- tests/e2e/pages/authentication.page.js
+++ tests/e2e/pages/authentication.page.js
@@ -1,28 +1,38 @@
-/**
-* @file authentication.page.js
-*/
 
 var EC = protractor.ExpectedConditions;
 
-var AuthenticationPage = function() {
+/**
+ * Authentication Page main constructor.
+ */
+function AuthenticationPage() {
+  var currentUser = null;
 
-  // Define authentication attributes.
   this.usernameField = $('input#edit-name');
   this.passwordField = $('input#edit-pass');
   this.loginButton   = $('input#edit-submit');
   this.loggedIn      = $('body.logged-in');
 
   this.login = function (user, password) {
+    if (user === currentUser) return; // Avoid login with same current logged user.
+    else this.logout(); // Perform logout previous to login.
+
     this.get();
     browser.wait(EC.visibilityOf(this.usernameField), browser.params.timeoutLimit);
     this.usernameField.sendKeys(user);
 
     this.passwordField.sendKeys(password);
     this.loginButton.click();
+
+    currentUser = user;
   };
 
+  /**
+   * Perform user logout.
+   * In case no user was logged in, avoid doing logout request.
+   */
   this.logout = function() {
-    browser.get('user/logout');
+    if (currentUser) browser.get('user/logout');
+    currentUser = null;
   }
 
   this.get = function() {
